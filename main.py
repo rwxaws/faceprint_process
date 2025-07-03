e8df7a70b08aef3faf8b4bbce9c4cff9751ff61a
rwxaws <pisceswolf96@gmail.com>
2025-07-03 15:12:15 +0300

    Remove old implementation

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from functions.filter import (
     filter_duty,
     filter_resttime,
     filter_restday,
-    filter_faceprint
 )
 from functions.extract_excuses import extract_excuses
 from functions.tables import get_excuses, get_no_excuses
@@ -30,7 +29,6 @@ def process_files(faceprint_file, rest_files, duty_file, date):
     filter_duty(con, target_date)
     filter_resttime(con, target_date)
     filter_restday(con, target_date)
-    filter_faceprint(con)
 
     extract_excuses(con)
     has_excuses = get_excuses(con)
@@ -41,13 +39,21 @@ def process_files(faceprint_file, rest_files, duty_file, date):
     rtl_text("بدون عذر", component="h2")
     st.dataframe(no_excuse, hide_index=True)
 
+
 rtl_text("معالجة بصمات الوجه", component="h1")
+
 rtl_text("اختر التاريخ", component="markdown")
 date = st.date_input('choose date', label_visibility="collapsed")
+
 rtl_text("ملف بصمة الوجه", component="markdown")
-faceprint_file = st.file_uploader('faceprint', type="csv", label_visibility="collapsed")
+faceprint_file = st.file_uploader('faceprint', type="csv",
+                                  label_visibility="collapsed")
+
 rtl_text("ملفات الاجازات والزمنيات", component="markdown")
-rest_files = st.file_uploader('vacation', type="xlsx", accept_multiple_files=True, label_visibility="collapsed")
+rest_files = st.file_uploader('rest', type="xlsx",
+                              accept_multiple_files=True,
+                              label_visibility="collapsed")
+
 rtl_text("ملف الواجبات", component="markdown")
 duty_file = st.file_uploader('duty', type="xlsx", label_visibility="collapsed")
 
