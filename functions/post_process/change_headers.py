f95110a4033b5edc8c495eba7db9a142f5c1b661
rwxaws <pisceswolf96@gmail.com>
2025-07-03 15:12:15 +0300

    Remove old implementation

--- functions/post_process/change_headers.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-def change_headers(df: pd.DataFrame, arabic_headers: list[str]) -> pd.DataFrame:
-    df.columns = arabic_headers
-    return df
--- functions/utils.py
+++ functions/utils.py
@@ -1,11 +1,5 @@
 import streamlit as st
-import json
 
-def read_json_data(json_file_path):
-    with open(json_file_path, "r", encoding='utf-8') as file:
-        data = json.load(file)
-
-    return data
 
 def rtl_text(text, component="markdown"):
     rtl_style = 'style="direction:rtl; text-align: right"'
@@ -14,6 +8,8 @@ def rtl_text(text, component="markdown"):
     if component == "markdown":
         st.markdown(f'<div {rtl_style}>{text}</div>', unsafe_allow_html=True)
     elif component == "h1":
-        st.markdown(f'<h1 {rtl_style_center}>{text}</h1>', unsafe_allow_html=True)
+        st.markdown(f'<h1 {rtl_style_center}>{text}</h1>',
+                    unsafe_allow_html=True)
     elif component == "h2":
-        st.markdown(f'<h2 {rtl_style_center}>{text}</h2>', unsafe_allow_html=True)
+        st.markdown(f'<h2 {rtl_style_center}>{text}</h2>',
+                    unsafe_allow_html=True)
